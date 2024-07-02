a1fc8b63d989fdb9ab9a6eb42e5e8d2c91fdf2f4
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-07-02 02:44:28 +0200

    minor Bufix

--- screen/BeefPage.jsx
+++ screen/BeefPage.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView,SafeAreaView, StyleSheet, View } from 'react-native';
 
-const BeefPage = ({
-    params,
-}) => (
-    <View>
-        <Text>BeefPage</Text>
-    </View>
+const BeefPage = ({}) => (
+
+
+
+
+
+    <SafeAreaView style={styles.container}>
+        <ScrollView>
+          <View>
+            hello world
+          </View>
+        </ScrollView>
+    </SafeAreaView>
 );
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: 'red',
+  }
+})
 
 export default BeefPage;
